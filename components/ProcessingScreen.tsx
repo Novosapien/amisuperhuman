77a38c46a9a77a7793f0bc9e1b2b84ba61580b4a
Brett StClair <jarvis@teraflow.digital>
2026-05-21 07:38:15 +0000

    chore: update processing screen header text

--- components/ProcessingScreen.tsx
+++ components/ProcessingScreen.tsx
@@ -134,7 +134,7 @@ export default function ProcessingScreen() {
           <div className="mac-dot red" />
           <div className="mac-dot yellow" />
           <div className="mac-dot green" />
-          <div className="mac-title">superhuman_advisor.py: running analysis</div>
+          <div className="mac-title">Our superhuman agentic AI bots are busy. This will take two to three minutes.</div>
         </div>
         <div className="mac-screen">
           <div id="code-output" ref={outputRef} />
